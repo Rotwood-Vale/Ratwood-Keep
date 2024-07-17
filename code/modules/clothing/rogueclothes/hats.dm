Rotwood-Vale/Ratwood-Keep

commit c089ec26cb1f3da1c709e97b7da1fe7a9b1b9032

Hoods can now be worn in the cloak slot, also red hood added to mage hood list (#337)
basically the title

it would be nice to have a covering that can go on the neck that doesn't
cover your tail - this does that! i also like the way the hood neck
thing looks on the neck but want a helmet with it 'cuz... why couldn't
you? was going to put it in the neck slot or mask slot like how you can
the kiffeyeh but felt it more fit in cloak slot given it can hide
identity

code/modules/clothing/rogueclothes/hats.dm
@@ -26,7 +26,7 @@
 	item_state = "basichood"
 	icon = 'icons/roguetown/clothing/head.dmi'
 	body_parts_covered = NECK
-	slot_flags = ITEM_SLOT_HEAD
+	slot_flags = ITEM_SLOT_HEAD|ITEM_SLOT_CLOAK
 	dynamic_hair_suffix = ""
 	edelay_type = 1
 	adjustable = CAN_CADJUST
@@ -145,7 +145,7 @@
 	..()
 
 /obj/item/clothing/head/roguetown/roguehood/mage/Initialize()
-	color = pick("#4756d8", "#759259", "#bf6f39", "#c1b144")
+	color = pick("#4756d8", "#759259", "#bf6f39", "#c1b144", "#b8252c")
 	..()
 
 /obj/item/clothing/head/roguetown/roguehood/AdjustClothes(mob/user)

commit 1251805512339cfbe4406aa380d883cbc5f7c2b9

quick fix for thing broken by #337

code/modules/clothing/rogueclothes/hats.dm
@@ -25,8 +25,11 @@
 	icon_state = "basichood"
 	item_state = "basichood"
 	icon = 'icons/roguetown/clothing/head.dmi'
+	mob_overlay_icon = 'icons/roguetown/clothing/onmob/head.dmi' //Overrides slot icon behavior
 	body_parts_covered = NECK
 	slot_flags = ITEM_SLOT_HEAD|ITEM_SLOT_CLOAK
+	sleevetype = null
+	sleeved = null
 	dynamic_hair_suffix = ""
 	edelay_type = 1
 	adjustable = CAN_CADJUST
@@ -160,6 +163,7 @@
 				var/mob/living/carbon/H = user
 				H.update_inv_head()
 				H.update_inv_wear_mask() //Snowflake case for Desert Merc hood
+				H.update_inv_cloak()
 			block2add = FOV_BEHIND
 		else if(adjustable == CADJUSTED)
 			ResetAdjust(user)
@@ -169,6 +173,7 @@
 					var/mob/living/carbon/H = user
 					H.update_inv_head()
 					H.update_inv_wear_mask() //Snowflake case for Desert Merc hood
+					H.update_inv_cloak()
 		user.update_fov_angles()
 
 