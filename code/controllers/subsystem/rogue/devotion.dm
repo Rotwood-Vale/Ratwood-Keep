Rotwood-Vale/Ratwood-Keep

commit 092c0d9bb3199625e952441fbcee88327187937f

Fix and balances (#1184)
alot in this, my usual mixed fix's and stuff

*werewolves after the PR was reverted that everyone had wanted reverted
for a long time... was way too overtuned, now before you have a stroke
this is on observation and feedback. infi stam for them was removed and
about everything from stats to armor was dialed back about 30% across
the board. so see where it is at and needs adjustment. this was to avoid
an outright disable till rework, you might be angry about it but they
could get the VL treatment... oh and I put an ear fuck timer on the play
global sound of there howls, first howl of the night from every werewolf
will be heard. then the sounds put on a 10min timer. the actual HOWL
talking is still only 1min. but no more sound spam.

*upcoming medical changes coming I did some groundwork on. end goal
tighten the gap in healing between doc and miricle spammers with
miracles eventually being slightly stronger. on this pass, sewing,
cauterizing and bandaging going thru clothes and I made the cauterizer
docs get not useless. its self heating now. toggle similar to a stun
mace.

*arteries are back up to a crit bleed out being bad. since you can now
bandage and sew thru clothing the arterial bleeds was brought back to
where they was before. embed chance for weapons on a crit droped from
75% to 40% and disembowling not happening possibly fixed back
to...actually working.

*devotion; made less painful to level up. 50% increase to baseline
prayer gains to all. break points for tiers lowered to gain the spells
faster. pestrans got start spells combo'd to diagnose AND lesser so they
dont have to pay for an eternity to get cure rot, its now tier 3.

*misc tweaks; adjusted purity prices for smokes and stuff, adjusted
stats by 1 for forge cleric and created a gimped shittier astra fire
spell for them to start with to light forges and shit with, gave an axed

code/controllers/subsystem/rogue/devotion.dm
@@ -6,10 +6,10 @@
 #define CLERIC_T4 4
 
 #define CLERIC_REQ_0 0
-#define CLERIC_REQ_1 100
-#define CLERIC_REQ_2 250
-#define CLERIC_REQ_3 500
-#define CLERIC_REQ_4 750
+#define CLERIC_REQ_1 75
+#define CLERIC_REQ_2 150
+#define CLERIC_REQ_3 350
+#define CLERIC_REQ_4 500
 
 // Cleric Holder Datums
 
@@ -33,7 +33,7 @@
 	/// How much progression is gained per process call
 	var/passive_progression_gain = 0
 	/// How much devotion is gained per prayer cycle
-	var/prayer_effectiveness = 2
+	var/prayer_effectiveness = 3
 	/// Spells we have granted thus far
 	var/list/granted_spells
 

code/datums/gods/patrons/divine_pantheon.dm
@@ -106,11 +106,10 @@
 	desc = "The Loving Daughter of Psydon, gifted man medicine."
 	worshippers = "The Sick, Phyicians, Apothecaries"
 	mob_traits = list(TRAIT_EMPATH, TRAIT_ROT_EATER)
-	t0 = /obj/effect/proc_holder/spell/invoked/diagnose
-	t1 = /obj/effect/proc_holder/spell/invoked/lesser_heal
-	t2 = /obj/effect/proc_holder/spell/invoked/heal
-	t3 = /obj/effect/proc_holder/spell/invoked/attach_bodypart
-	t4 = /obj/effect/proc_holder/spell/invoked/cure_rot
+	t0 = list(/obj/effect/proc_holder/spell/invoked/diagnose, /obj/effect/proc_holder/spell/invoked/lesser_heal) // Combine both spells on t0
+	t1 = /obj/effect/proc_holder/spell/invoked/heal
+	t2 = /obj/effect/proc_holder/spell/invoked/attach_bodypart
+	t3 = /obj/effect/proc_holder/spell/invoked/cure_rot
 	confess_lines = list(
 		"PESTRA SOOTHES ALL ILLS!",
 		"DECAY IS A CONTINUATION OF LIFE!",

code/datums/wounds/types/arteries.dm
@@ -6,7 +6,7 @@
 	sound_effect = 'sound/combat/crit.ogg'
 	whp = 50
 	sewn_whp = 20
-	bleed_rate = 10
+	bleed_rate = 20
 	sewn_bleed_rate = 0.2
 	clotting_threshold = null
 	sewn_clotting_threshold = null
@@ -18,7 +18,7 @@
 	can_cauterize = TRUE
 	critical = TRUE
 	sleep_healing = 0
-	embed_chance = 75
+	embed_chance = 40
 
 	zombie_infection_probability = 50
 	werewolf_infection_probability = 50

code/datums/wounds/types/slashes.dm
@@ -61,7 +61,7 @@
 	/// Organs we can disembowel associated with chance to disembowel
 	var/static/list/affected_organs = list(
 		ORGAN_SLOT_STOMACH = 100,
-		ORGAN_SLOT_LIVER = 50,
+		ORGAN_SLOT_LIVER = 100,
 	)
 
 /datum/wound/slash/disembowel/can_stack_with(datum/wound/other)

code/game/objects/items/rogueitems/natural/clothfibersthorn.dm
@@ -180,9 +180,6 @@
 	var/obj/item/bodypart/affecting = H.get_bodypart(check_zone(user.zone_selected))
 	if(!affecting)
 		return
-	if(!get_location_accessible(H, check_zone(user.zone_selected)))
-		to_chat(user, span_warning("Something in the way."))
-		return
 	if(affecting.bandage)
 		to_chat(user, span_warning("There is already a bandage."))
 		return

code/game/objects/items/rogueitems/needle.dm
@@ -106,9 +106,6 @@
 	if(stringamt < 1)
 		to_chat(user, span_warning("The needle has no thread left!"))
 		return
-	if(!get_location_accessible(patient, check_zone(doctor.zone_selected)))
-		to_chat(doctor, span_warning("Something in the way."))
-		return FALSE
 	var/list/sewable
 	var/obj/item/bodypart/affecting
 	if(iscarbon(patient))

code/modules/antagonists/roguetown/villain/werewolf/werewolf.dm
@@ -121,7 +121,7 @@
 	desc = ""
 	icon_state = null
 	body_parts_covered = FULL_BODY
-	armor = list("blunt" = 100, "slash" = 90, "stab" = 80, "bullet" = 70, "laser" = 0,"energy" = 0, "bomb" = 0, "bio" = 0, "rad" = 0, "fire" = 0, "acid" = 0)
+	armor = list("blunt" = 80, "slash" = 60, "stab" = 60, "bullet" = 60, "laser" = 0,"energy" = 0, "bomb" = 0, "bio" = 0, "rad" = 0, "fire" = 0, "acid" = 0)
 	prevent_crits = list(BCLASS_CUT, BCLASS_CHOP, BCLASS_STAB, BCLASS_BLUNT, BCLASS_TWIST)
 	blocksound = SOFTHIT
 	blade_dulling = DULLING_BASHCHOP
@@ -136,7 +136,7 @@
 	attack_verb = list("claws", "mauls", "eviscerates")
 	animname = "chop"
 	hitsound = "genslash"
-	penfactor = 50
+	penfactor = 30
 	candodge = TRUE
 	canparry = TRUE
 	miss_text = "slashes the air!"
@@ -150,12 +150,12 @@
 	lefthand_file = null
 	righthand_file = null
 	icon = 'icons/roguetown/weapons/32.dmi'
-	max_blade_int = 900
-	max_integrity = 900
+	max_blade_int = -1
+	max_integrity = -1
 	force = 25
 	block_chance = 0
 	wdefense = 4
-	armor_penetration = 15
+	armor_penetration = 20
 	associated_skill = /datum/skill/combat/unarmed
 	wlength = WLENGTH_NORMAL
 	w_class = WEIGHT_CLASS_BULKY

code/modules/antagonists/roguetown/villain/werewolf/werewolf_spells.dm
@@ -1,65 +1,73 @@
+var/last_howl_sound_time = -99999 // Set to a time far in the past so the initial howl can play immediately
+var/sound_cooldown = 10 * 60 * 10 // cooldown in deciseconds (15 minutes)
+
 /obj/effect/proc_holder/spell/self/howl
-	name = "Howl"
-	desc = "!"
-	overlay_state = "howl"
-	antimagic_allowed = TRUE
-	charge_max = 600 //1 minute
+    name = "Howl"
+    desc = "!"
+    overlay_state = "howl"
+    antimagic_allowed = TRUE
+    charge_max = 600 // 1 minute
 
 /obj/effect/proc_holder/spell/self/howl/cast(mob/user = usr)
-	..()
-	var/message = input("Howl at the hidden moon", "WEREWOLF") as text|null
-	if(!message) return
+    ..()
+    var/message = input("Howl at the hidden moon", "WEREWOLF") as text|null
+    if (!message) return
 
-	var/datum/antagonist/werewolf/werewolf_player = user.mind.has_antag_datum(/datum/antagonist/werewolf)
+    var/datum/antagonist/werewolf/werewolf_player = user.mind.has_antag_datum(/datum/antagonist/werewolf)
+    
+    // sound played for owner
+    playsound(src, pick('sound/vo/mobs/wwolf/howl (1).ogg','sound/vo/mobs/wwolf/howl (2).ogg'), 100, TRUE)
 
-	// sound played for owner
-	playsound(src, pick('sound/vo/mobs/wwolf/howl (1).ogg','sound/vo/mobs/wwolf/howl (2).ogg'), 100, TRUE)
-	
-	for(var/mob/player in GLOB.player_list)
+    for (var/mob/player in GLOB.player_list)
+        if (!player.mind) continue
+        if (player.stat == DEAD) continue
+        if (isbrain(player)) continue
 
-		if(!player.mind) continue
-		if(player.stat == DEAD) continue
-		if(isbrain(player)) continue
+        // Announcement to other werewolves
+        if (player.mind.has_antag_datum(/datum/antagonist/werewolf))
+            to_chat(player, span_boldannounce("[werewolf_player.wolfname] howls: [message]"))
 
-		// Announcement to other werewolves
-		if(player.mind.has_antag_datum(/datum/antagonist/werewolf))
-			to_chat(player, span_boldannounce("[werewolf_player.wolfname] howls: [message]"))
+        // sound played for other players
+        var/current_time = world.time // Define current_time
 
-		//sound played for other players
-		if(player == src) continue
-		if(get_dist(player, src) > 7)
-			player.playsound_local(get_turf(player), pick('sound/vo/mobs/wwolf/howldist (1).ogg','sound/vo/mobs/wwolf/howldist (2).ogg'), 100, FALSE, pressure_affected = FALSE)
+        if (player == src) continue
+        if (get_dist(player, src) > 7) continue
 
-	user.log_message("howls: [message] (WEREWOLF)")
+        if (current_time - last_howl_sound_time < sound_cooldown) // stop ear spamming
+            continue
+
+        last_howl_sound_time = current_time
+        player.playsound_local(get_turf(player), pick('sound/vo/mobs/wwolf/howldist (1).ogg','sound/vo/mobs/wwolf/howldist (2).ogg'), 100, FALSE, pressure_affected = FALSE)
+
+    user.log_message("howls: [message] (WEREWOLF)")
 
 /obj/effect/proc_holder/spell/self/claws
-	name = "Lupine Claws"
-	desc = "!"
-	overlay_state = "claws"
-	antimagic_allowed = TRUE
-	charge_max = 20 //2 seconds
-	var/extended = FALSE
+    name = "Lupine Claws"
+    desc = "!"
+    overlay_state = "claws"
+    antimagic_allowed = TRUE
+    charge_max = 20 // 2 seconds
+    var/extended = FALSE
 
 /obj/effect/proc_holder/spell/self/claws/cast(mob/user = usr)
-	..()
-	var/obj/item/rogueweapon/werewolf_claw/left/l
-	var/obj/item/rogueweapon/werewolf_claw/right/r
+    ..()
+    var/obj/item/rogueweapon/werewolf_claw/left/l
+    var/obj/item/rogueweapon/werewolf_claw/right/r
+    l = user.get_active_held_item()
+    r = user.get_inactive_held_item()
 
-	l = user.get_active_held_item()
-	r = user.get_inactive_held_item()
-	if(extended)
-		if(istype(user.get_active_held_item(), /obj/item/rogueweapon/werewolf_claw))
-			user.dropItemToGround(l, TRUE)
-			user.dropItemToGround(r, TRUE)
-			qdel(l)
-			qdel(r)
-			//user.visible_message("Your claws retract.", "You feel your claws retracting.", "You hear a sound of claws retracting.")
-			extended = FALSE
-	else
-		l = new(user,1)
-		r = new(user,2)
-		user.put_in_hands(l, TRUE, FALSE, TRUE)
-		user.put_in_hands(r, TRUE, FALSE, TRUE)
-		//user.visible_message("Your claws extend.", "You feel your claws extending.", "You hear a sound of claws extending.")
-		extended = TRUE
-	
+    if (extended)
+        if (istype(user.get_active_held_item(), /obj/item/rogueweapon/werewolf_claw))
+            user.dropItemToGround(l, TRUE)
+            user.dropItemToGround(r, TRUE)
+            qdel(l)
+            qdel(r)
+        // user.visible_message("Your claws retract.", "You feel your claws retracting.", "You hear a sound of claws retracting.")
+        extended = FALSE
+    else
+        l = new(user, 1)
+        r = new(user, 2)
+        user.put_in_hands(l, TRUE, FALSE, TRUE)
+        user.put_in_hands(r, TRUE, FALSE, TRUE)
+        // user.visible_message("Your claws extend.", "You feel your claws extending.", "You hear a sound of claws extending.")
+        extended = TRUE

commit 15d64463132dc3967d657f7b27e25d979d7c8ab5

Pestra Cleric/Acolyte Fix (#1206)
<!-- Write **BELOW** The Headers and **ABOVE** The comments else it may
not be viewable. -->

## About The Pull Request

Fixes a bug caused by #1184 where people wouldn't get Diagnose and
lesser miracle due to a list being inside a list.


![image](https://github.com/user-attachments/assets/01f747a5-29db-444a-ab0f-0abbd4faf5c1)

![image](https://github.com/user-attachments/assets/4e5c3545-1c56-4a2e-be66-7d8ab1abdc76)

## Why It's Good For The Game

bug fix

code/controllers/subsystem/rogue/devotion.dm
@@ -102,8 +102,12 @@
 /datum/devotion/proc/grant_spells(mob/living/carbon/human/H)
 	if(!H || !H.mind || !patron)
 		return
-
-	var/list/spelllist = list(patron.t0, patron.t1)
+	var/list/spelllist = list()
+	if(islist(patron.t0)) // Snowflake code to check if T0 is a list, necessary for pestra
+		spelllist += patron.t0
+	else if (patron.t0)
+		spelllist += list(patron.t0)
+	spelllist += list(patron.t1)
 	for(var/spell_type in spelllist)
 		if(!spell_type || H.mind.has_spell(spell_type))
 			continue