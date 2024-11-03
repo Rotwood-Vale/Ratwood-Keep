Rotwood-Vale/Ratwood-Keep

commit 7fb5d3dbac0b17aea70e42c0149e1da8326d1a65

Now the announcements will use the proper titles (#1375)
<!-- Write **BELOW** The Headers and **ABOVE** The comments else it may
not be viewable. -->

## About The Pull Request
Made the throne now store the title of the person using it's commands.

![announcemet](https://github.com/user-attachments/assets/943a94a1-06e2-4076-9fff-d5e99c269274)

![Outlaw](https://github.com/user-attachments/assets/041c648a-6223-4b5d-8387-f37271648f19)

![Decree](https://github.com/user-attachments/assets/6abe11e9-f3c6-4b1b-9863-27774f8ede98)

![Taxes](https://github.com/user-attachments/assets/a86ac5f1-bb45-436d-b561-2e2a8cb6e341)
![Scom
decree](https://github.com/user-attachments/assets/e7e29571-c05e-4e6f-9318-3ae9c18238bc)

<!-- Describe The Pull Request. Please be sure every change is
documented or this can delay review and even discourage maintainers from
merging your PR! -->

## Why It's Good For The Game
It fixes #1251
<!-- Argue for the merits of your changes and how they benefit the game,
especially if they are controversial and/or far reaching. If you can't
actually explain WHY what you are doing will improve the game, then it
probably isn't good for the game in the first place. -->

code/modules/jobs/job_types/roguetown/nobility/marshal.dm
@@ -115,18 +115,19 @@
 /mob/living/carbon/human/proc/request_outlaw()
 	set name = "Request Outlaw"
 	set category = "Martial Law"
+	var/title = src.get_role_title()
 	if(stat)
 		return
 	var/inputty = input("Outlaw a person", "MARTIAL LAW") as text|null
 	if(inputty)
 		if(hasomen(OMEN_NOLORD))
-			make_outlaw(inputty)
+			make_outlaw(inputty, title)
 		else
 			var/lord = find_lord()
 			if(lord)
 				INVOKE_ASYNC(GLOBAL_PROC, GLOBAL_PROC_REF(lord_outlaw_requested), src, lord, inputty)
 			else
-				make_outlaw(inputty)
+				make_outlaw(inputty, title)
 				
 /proc/find_lord(required_stat = CONSCIOUS)
 	var/mob/living/lord

code/modules/roguetown/roguemachine/titan.dm
@@ -1,6 +1,7 @@
 GLOBAL_LIST_EMPTY(outlawed_players)
 GLOBAL_LIST_EMPTY(lord_decrees)
 GLOBAL_LIST_INIT(laws_of_the_land, initialize_laws_of_the_land())
+/var/atom/TITLE_LORD //outside var to relay to all scomms if there is a Duke or Duchess
 
 /proc/initialize_laws_of_the_land()
 	var/list/laws = strings("laws_of_the_land.json", "lawsets")
@@ -23,6 +24,7 @@ GLOBAL_LIST_INIT(laws_of_the_land, initialize_laws_of_the_land())
 	flags_1 = HEAR_1
 	anchored = TRUE
 	var/mode = 0
+	var/title_D
 	COOLDOWN_DECLARE(king_announcement)
 
 /obj/structure/roguemachine/titan/obj_break(damage_flag)
@@ -57,6 +59,7 @@ GLOBAL_LIST_INIT(laws_of_the_land, initialize_laws_of_the_land())
 		return
 	var/mob/living/carbon/human/H = speaker
 	var/nocrown
+	title_D = H.get_role_title()
 	if(!istype(H.head, /obj/item/clothing/head/roguetown/crown/serpcrown))
 		nocrown = TRUE
 	var/notlord
@@ -224,7 +227,7 @@ GLOBAL_LIST_INIT(laws_of_the_land, initialize_laws_of_the_land())
 			return
 		newtax = CLAMP(newtax, 1, 99)
 		SStreasury.tax_value = newtax / 100
-		priority_announce("The new tax in Rockhill shall be [newtax] percent.", "The Generous Duke Decrees", pick('sound/misc/royal_decree.ogg', 'sound/misc/royal_decree2.ogg'), "Captain")
+		priority_announce("The new tax in Rockhill shall be [newtax] percent.", "The Generous [title_D] Decrees", pick('sound/misc/royal_decree.ogg', 'sound/misc/royal_decree2.ogg'), "Captain")
 
 
 /obj/structure/roguemachine/titan/proc/make_announcement(mob/living/user, raw_message)
@@ -263,6 +266,7 @@ GLOBAL_LIST_INIT(laws_of_the_land, initialize_laws_of_the_land())
 		return
 
 	GLOB.lord_decrees += raw_message
+	TITLE_LORD = title_D
 	try_make_rebel_decree(user)
 
 	SScommunications.make_announcement(user, TRUE, raw_message)
@@ -275,15 +279,15 @@ GLOBAL_LIST_INIT(laws_of_the_land, initialize_laws_of_the_land())
 			return
 	else
 		return
-	return make_outlaw(raw_message)
+	return make_outlaw(raw_message, title_D)
 
-/proc/make_outlaw(raw_message, silent = FALSE)
+/proc/make_outlaw(raw_message, title_D, silent = FALSE)
 	var/sanitized_name = html_encode(raw_message)  // Escapes special characters
 
 	if(sanitized_name in GLOB.outlawed_players)
 		GLOB.outlawed_players -= sanitized_name
 		if(!silent)
-			priority_announce("[sanitized_name] is no longer an outlaw in Rockhill lands.", "The Duke Decrees", 'sound/misc/royal_decree.ogg', "Captain")
+			priority_announce("[sanitized_name] is no longer an outlaw in Rockhill lands.", "The [title_D] Decrees", 'sound/misc/royal_decree.ogg', "Captain")
 		return FALSE
 
 	var/found = FALSE
@@ -297,7 +301,7 @@ GLOBAL_LIST_INIT(laws_of_the_land, initialize_laws_of_the_land())
 
 	GLOB.outlawed_players += sanitized_name
 	if(!silent)
-		priority_announce("[sanitized_name] has been declared an outlaw and must be captured or slain.", "The Duke Decrees", 'sound/misc/royal_decree2.ogg', "Captain")
+		priority_announce("[sanitized_name] has been declared an outlaw and must be captured or slain.", "The [title_D] Decrees", 'sound/misc/royal_decree2.ogg', "Captain")
 	return TRUE
 
 /proc/make_law(raw_message)

code/modules/roguetown/roguemachine/scomm.dm
@@ -39,7 +39,7 @@
 	if(world.time > next_decree)
 		next_decree = world.time + rand(3 MINUTES, 8 MINUTES)
 		if(GLOB.lord_decrees.len)
-			say("The Duke Decrees: [pick(GLOB.lord_decrees)]", spans = list("info"))
+			say("The [TITLE_LORD] Decrees: [pick(GLOB.lord_decrees)]", spans = list("info"))
 
 /obj/structure/roguemachine/scomm/attack_hand(mob/living/user)
 	. = ..()

commit 483d74015a4cabe23b10bf4dd17eb6e8da204b96

Changes how the title of the Lord is stored for announcements (#1399)
<!-- Write **BELOW** The Headers and **ABOVE** The comments else it may
not be viewable. -->

## About The Pull Request
Now it's all stored in a global var that is already inizialiced with
"Duke" in case any of the announcements are called outside of the
Throne.
This var will now only change when the priest coronates a new ruler.


![Duchess](https://github.com/user-attachments/assets/acc7295e-fcaa-4929-ad2d-2cfd7701313c)

![Duke](https://github.com/user-attachments/assets/85a9a51d-9b7a-41d0-80eb-8594127c6dd9)

<!-- Describe The Pull Request. Please be sure every change is
documented or this can delay review and even discourage maintainers from
merging your PR! -->

## Why It's Good For The Game
Fixes an issue introduced with #1375, that caused the outlaw special
trait to make an announcement with a wrong title.


![image](https://github.com/user-attachments/assets/3dc06ae9-dddf-4cdd-b94a-57d059846529)
<!-- Argue for the merits of your changes and how they benefit the game,
especially if they are controversial and/or far reaching. If you can't
actually explain WHY what you are doing will improve the game, then it
probably isn't good for the game in the first place. -->

code/modules/jobs/job_types/roguetown/church/priest.dm
@@ -118,6 +118,7 @@
 		removeomen(OMEN_NOLORD)
 		say("By the authority of the gods, I pronounce you [SSticker.rulertype] of Rockhill!")
 		priority_announce("[real_name] the [dispjob] has named [HU.real_name] the [SSticker.rulertype] of Rockhill!", title = "Long Live [HU.real_name]!", sound = 'sound/misc/bell.ogg')
+		TITLE_LORD = SSticker.rulertype
 
 /mob/living/carbon/human/proc/churchexcommunicate()
 	set name = "Excommunicate"

code/modules/jobs/job_types/roguetown/nobility/marshal.dm
@@ -121,13 +121,13 @@
 	var/inputty = input("Outlaw a person", "MARTIAL LAW") as text|null
 	if(inputty)
 		if(hasomen(OMEN_NOLORD))
-			make_outlaw(inputty, title)
+			make_outlaw(inputty)
 		else
 			var/lord = find_lord()
 			if(lord)
 				INVOKE_ASYNC(GLOBAL_PROC, GLOBAL_PROC_REF(lord_outlaw_requested), src, lord, inputty)
 			else
-				make_outlaw(inputty, title)
+				make_outlaw(inputty)
 				
 /proc/find_lord(required_stat = CONSCIOUS)
 	var/mob/living/lord

code/modules/roguetown/roguemachine/titan.dm
@@ -1,7 +1,7 @@
 GLOBAL_LIST_EMPTY(outlawed_players)
 GLOBAL_LIST_EMPTY(lord_decrees)
 GLOBAL_LIST_INIT(laws_of_the_land, initialize_laws_of_the_land())
-/var/atom/TITLE_LORD //outside var to relay to all scomms if there is a Duke or Duchess
+/var/atom/TITLE_LORD = "Duke" //outside var to relay to all announcements if there is a Duke or Duchess
 
 /proc/initialize_laws_of_the_land()
 	var/list/laws = strings("laws_of_the_land.json", "lawsets")
@@ -24,7 +24,6 @@ GLOBAL_LIST_INIT(laws_of_the_land, initialize_laws_of_the_land())
 	flags_1 = HEAR_1
 	anchored = TRUE
 	var/mode = 0
-	var/title_D
 	COOLDOWN_DECLARE(king_announcement)
 
 /obj/structure/roguemachine/titan/obj_break(damage_flag)
@@ -59,7 +58,6 @@ GLOBAL_LIST_INIT(laws_of_the_land, initialize_laws_of_the_land())
 		return
 	var/mob/living/carbon/human/H = speaker
 	var/nocrown
-	title_D = H.get_role_title()
 	if(!istype(H.head, /obj/item/clothing/head/roguetown/crown/serpcrown))
 		nocrown = TRUE
 	var/notlord
@@ -227,7 +225,7 @@ GLOBAL_LIST_INIT(laws_of_the_land, initialize_laws_of_the_land())
 			return
 		newtax = CLAMP(newtax, 1, 99)
 		SStreasury.tax_value = newtax / 100
-		priority_announce("The new tax in Rockhill shall be [newtax] percent.", "The Generous [title_D] Decrees", pick('sound/misc/royal_decree.ogg', 'sound/misc/royal_decree2.ogg'), "Captain")
+		priority_announce("The new tax in Rockhill shall be [newtax] percent.", "The Generous [TITLE_LORD] Decrees", pick('sound/misc/royal_decree.ogg', 'sound/misc/royal_decree2.ogg'), "Captain")
 
 
 /obj/structure/roguemachine/titan/proc/make_announcement(mob/living/user, raw_message)
@@ -266,7 +264,6 @@ GLOBAL_LIST_INIT(laws_of_the_land, initialize_laws_of_the_land())
 		return
 
 	GLOB.lord_decrees += raw_message
-	TITLE_LORD = title_D
 	try_make_rebel_decree(user)
 
 	SScommunications.make_announcement(user, TRUE, raw_message)
@@ -279,15 +276,15 @@ GLOBAL_LIST_INIT(laws_of_the_land, initialize_laws_of_the_land())
 			return
 	else
 		return
-	return make_outlaw(raw_message, title_D)
+	return make_outlaw(raw_message)
 
-/proc/make_outlaw(raw_message, title_D, silent = FALSE)
+/proc/make_outlaw(raw_message, silent = FALSE)
 	var/sanitized_name = html_encode(raw_message)  // Escapes special characters
 
 	if(sanitized_name in GLOB.outlawed_players)
 		GLOB.outlawed_players -= sanitized_name
 		if(!silent)
-			priority_announce("[sanitized_name] is no longer an outlaw in Rockhill lands.", "The [title_D] Decrees", 'sound/misc/royal_decree.ogg', "Captain")
+			priority_announce("[sanitized_name] is no longer an outlaw in Rockhill lands.", "The [TITLE_LORD] Decrees", 'sound/misc/royal_decree.ogg', "Captain")
 		return FALSE
 
 	var/found = FALSE
@@ -301,7 +298,7 @@ GLOBAL_LIST_INIT(laws_of_the_land, initialize_laws_of_the_land())
 
 	GLOB.outlawed_players += sanitized_name
 	if(!silent)
-		priority_announce("[sanitized_name] has been declared an outlaw and must be captured or slain.", "The [title_D] Decrees", 'sound/misc/royal_decree2.ogg', "Captain")
+		priority_announce("[sanitized_name] has been declared an outlaw and must be captured or slain.", "The [TITLE_LORD] Decrees", 'sound/misc/royal_decree2.ogg', "Captain")
 	return TRUE
 
 /proc/make_law(raw_message)