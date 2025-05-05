noelle-lavenza/Azure-Peak

Rewrite NPC pathfinding

code/game/turfs/open/floor/roguefloor.dm
@@ -363,23 +363,21 @@
 	var/dirt_amt = 3
 
 /turf/open/floor/rogue/dirt/get_slowdown(mob/user)
-	var/returned = slowdown
+	. = slowdown
 	var/negate_slowdown = FALSE
+	if(isliving(user))
+		var/mob/living/living_user = user
+		for(var/obj/item/stick in living_user.held_items)
+			if(stick.walking_stick && !stick.wielded && !living_user.cmode)
+				negate_slowdown = TRUE
 
-	for(var/obj/item/I in user.held_items)
-		if(I.walking_stick)
-			if(!I.wielded)
-				var/mob/living/L = user
-				if(!L.cmode)
-					negate_slowdown = TRUE
-
-	if(HAS_TRAIT(user, TRAIT_LONGSTRIDER))
-		negate_slowdown = TRUE
+		if(HAS_TRAIT(user, TRAIT_LONGSTRIDER))
+			negate_slowdown = TRUE
 
 	if(negate_slowdown)
-		returned = max(returned-2, 0)
+		. -= 2
 
-	return returned
+	return max(., 0)
 
 
 /turf/open/floor/rogue/dirt/attack_right(mob/user)

Rotwood-Vale/Ratwood-Keep

code/game/turfs/open/floor/roguefloor.dm
@@ -391,19 +391,17 @@
 	if(user.is_floor_hazard_immune())
 		return 0
 
-	var/returned = slowdown
+	. = slowdown
 	var/negate_slowdown = FALSE
-	for(var/obj/item/I in user.held_items)
-		if(I.walking_stick)
-			if(!I.wielded)
-				var/mob/living/L = user
-				if(!L.cmode)
-					negate_slowdown = TRUE
+	for(var/obj/item/stick in user.held_items)
+		if(stick.walking_stick && !stick.wielded && !user.cmode)
+			negate_slowdown = TRUE
+			break
 	if(HAS_TRAIT(user, TRAIT_BOG_TREKKING))
 		negate_slowdown = TRUE
 	if(negate_slowdown)
-		returned = max(returned-2, 0)
-	return returned
+		. -= 2
+	return max(., 0)
 
 
 /turf/open/floor/rogue/dirt/attack_right(mob/user)