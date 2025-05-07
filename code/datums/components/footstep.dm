Rotwood-Vale/Ratwood-Keep

commit 4a492a0e66bacb7f97e85aef1ca3301d208b9f13

Premium slaves and exotic silks (#2345)
<!-- Write **BELOW** The Headers and **ABOVE** The comments else it may
not be viewable. -->

## About The Pull Request

steals exotic silks and anklets from dreamkeep and gives them to the
zybantine slave and for the merchant to buy, also adds a bit of code to
make sole-less shoes work
<!-- Describe The Pull Request. Please be sure every change is
documented or this can delay review and even discourage maintainers from
merging your PR! -->

## Why It's Good For The Game
more variety and identity for regions, also more end-game stuff to spend
your money on, can't go wrong with either, also the zybantine slaves are
virgins, thus premium cargo, it makes zero sense for them to be dressed
in shitty rags.
<!-- Argue for the merits of your changes and how they benefit the game,
especially if they are controversial and/or far reaching. If you can't
actually explain WHY what you are doing will improve the game, then it
probably isn't good for the game in the first place. -->


## Proof of Testing (Required)

![image](https://github.com/user-attachments/assets/5724877f-e583-44dc-bf44-3bd4713dc7e2)

<!-- Show proof of testing, screenshots or recordings for features,
proof of compile for backend changes -->

code/datums/components/footstep.dm
@@ -107,37 +107,39 @@
 	var/list/used_footsteps
 
 	if(H.shoes || feetCover) //are we wearing shoes
-		//SANITY CHECK, WILL NOT PLAY A SOUND IF THE LIST IS INVALID
-		if(!GLOB.footstep[T.footstep] || (LAZYLEN(GLOB.footstep[T.footstep]) < 3))
-			CRASH("Invalid footstep value. Turf type: [T.type]; Value: [T.footstep]")
-			return
-		used_footsteps = GLOB.footstep[T.footstep][1]
-		used_footsteps = used_footsteps.Copy()
-		used_sound = pick_n_take(used_footsteps)
-		if(used_sound == last_sound)
-			if(used_footsteps.len)
+		var/obj/item/clothing/shoes/humshoes = H.shoes
+		if(humshoes.isbarefoot == FALSE) //are the shoes actually not barefoot
+			//SANITY CHECK, WILL NOT PLAY A SOUND IF THE LIST IS INVALID
+			if(!GLOB.footstep[T.footstep] || (LAZYLEN(GLOB.footstep[T.footstep]) < 3))
+				CRASH("Invalid footstep value. Turf type: [T.type]; Value: [T.footstep]")
+				return
+			used_footsteps = GLOB.footstep[T.footstep][1]
+			used_footsteps = used_footsteps.Copy()
+			used_sound = pick_n_take(used_footsteps)
+			if(used_sound == last_sound)
+				if(used_footsteps.len)
+					used_sound = pick(used_footsteps)
+			if(!used_sound)
+				used_sound = last_sound
+			last_sound = used_sound
+			playsound(T, used_sound,
+				GLOB.footstep[T.footstep][2],
+				FALSE,
+				GLOB.footstep[T.footstep][3] + e_range)
+		else
+			//SANITY CHECK, WILL NOT PLAY A SOUND IF THE LIST IS INVALID
+			if(!GLOB.barefootstep[T.barefootstep] || (LAZYLEN(GLOB.barefootstep[T.barefootstep]) < 3))
+				CRASH("Invalid barefootstep value given. Turf type: [T.type]; Value: [T.barefootstep]")
+				return
+			used_footsteps = GLOB.barefootstep[T.barefootstep][1]
+			used_footsteps = used_footsteps.Copy()
+			used_sound = pick_n_take(used_footsteps)
+			if(used_sound == last_sound)
 				used_sound = pick(used_footsteps)
-		if(!used_sound)
-			used_sound = last_sound
-		last_sound = used_sound
-		playsound(T, used_sound,
-			GLOB.footstep[T.footstep][2],
-			FALSE,
-			GLOB.footstep[T.footstep][3] + e_range)
-	else
-		//SANITY CHECK, WILL NOT PLAY A SOUND IF THE LIST IS INVALID
-		if(!GLOB.barefootstep[T.barefootstep] || (LAZYLEN(GLOB.barefootstep[T.barefootstep]) < 3))
-			CRASH("Invalid barefootstep value given. Turf type: [T.type]; Value: [T.barefootstep]")
-			return
-		used_footsteps = GLOB.barefootstep[T.barefootstep][1]
-		used_footsteps = used_footsteps.Copy()
-		used_sound = pick_n_take(used_footsteps)
-		if(used_sound == last_sound)
-			used_sound = pick(used_footsteps)
-		if(!used_sound)
-			used_sound = last_sound
-		last_sound = used_sound
-		playsound(T, used_sound,
-			GLOB.barefootstep[T.barefootstep][2],
-			TRUE,
-			GLOB.barefootstep[T.barefootstep][3] + e_range)
+			if(!used_sound)
+				used_sound = last_sound
+			last_sound = used_sound
+			playsound(T, used_sound,
+				GLOB.barefootstep[T.barefootstep][2],
+				TRUE,
+				GLOB.barefootstep[T.barefootstep][3] + e_range)

code/datums/migrants/migrant_waves/slaver roles.dm
@@ -218,14 +218,15 @@
 	show_wanderer_examine = FALSE
 	show_foreign_examine = TRUE
 
-/datum/outfit/job/roguetown/slaver/slavez/pre_equip(mob/living/carbon/human/H)
+/datum/outfit/job/roguetown/slaver/slavez/pre_equip(mob/living/carbon/human/H) 
 	..()
-	armor = /obj/item/clothing/suit/roguetown/shirt/rags
+	shirt = /obj/item/clothing/suit/roguetown/shirt/exoticsilkbra 
 	neck = /obj/item/clothing/neck/roguetown/collar/leather/cursed
-	belt = /obj/item/storage/belt/rogue/leather/rope
+	belt = /obj/item/storage/belt/rogue/leather/exoticsilkbelt
 	beltl = /obj/item/storage/belt/rogue/pouch
 	beltr = /obj/item/flint
-	shoes = /obj/item/clothing/shoes/roguetown/shortboots
+	shoes = /obj/item/clothing/shoes/roguetown/anklets
+	mask = /obj/item/clothing/mask/rogue/exoticsilkmask
 	if(H.mind)
 		H.virginity = TRUE
 		H.mind.adjust_skillrank(/datum/skill/combat/wrestling, 1, TRUE)

code/modules/cargo/packsrogue/wardrobe.dm
@@ -188,3 +188,13 @@
 					/obj/item/clothing/suit/roguetown/shirt/dress/velvetdress,
 					/obj/item/clothing/suit/roguetown/shirt/dress/velvetdress,
 				)
+
+/datum/supply_pack/rogue/wardrobe/exoticpack
+	name = "Exotic Silks Pack"
+	cost = 250
+	contains = list(
+					/obj/item/clothing/shoes/roguetown/anklets,
+					/obj/item/clothing/mask/rogue/exoticsilkmask,
+					/obj/item/clothing/suit/roguetown/shirt/exoticsilkbra,
+					/obj/item/storage/belt/rogue/leather/exoticsilkbelt,
+				)

code/modules/clothing/rogueclothes/mask.dm
@@ -169,3 +169,11 @@
 	max_integrity = 150
 	sellprice = 100
 	smeltresult = /obj/item/ingot/gold
+
+/obj/item/clothing/mask/rogue/exoticsilkmask
+	name = "Exotic Silk Mask"
+	icon_state = "exoticsilkmask"
+	flags_inv = HIDEFACE|HIDEFACIALHAIR
+	body_parts_covered = NECK|MOUTH
+	slot_flags = ITEM_SLOT_MASK|ITEM_SLOT_HIP
+	sewrepair = TRUE

code/modules/clothing/rogueclothes/shirts.dm
@@ -392,3 +392,12 @@
 		if(get_detail_color())
 			pic.color = get_detail_color()
 		add_overlay(pic)
+
+/obj/item/clothing/suit/roguetown/shirt/exoticsilkbra
+	name = "Exotic Silks"
+	desc = "Fanciful gold laced silks barely able to conceal what little it covers."
+	icon_state = "exoticsilkbra"
+	item_state = "exoticsilkbra"
+	gender = PLURAL
+	body_parts_covered = CHEST|ARMS|VITALS
+	sewrepair = TRUE

code/modules/clothing/rogueclothes/shoes.dm
@@ -72,6 +72,13 @@
 	resistance_flags = null
 	color = "#cbcac9"
 
+/obj/item/clothing/shoes/roguetown/anklets
+	name = "Gold Anklets"
+	desc = "Delicate golden hoops worn around the ankle."
+	icon_state = "anklets"
+	item_state = "anklets"
+	isbarefoot = TRUE
+	anvilrepair = /datum/skill/craft/blacksmithing
 
 // **ARMOURED** //
 

code/modules/clothing/rogueclothes/storage.dm
@@ -349,6 +349,13 @@
 		STR.max_items = 14
 		STR.not_while_equipped = TRUE
 		STR.allow_dump_out = TRUE
+
+/obj/item/storage/belt/rogue/leather/exoticsilkbelt
+	name = "Exotic Silk Belt"
+	desc = "A gold adorned belt with the softest of silks barely concealing one's bits."
+	icon_state = "exoticsilkbelt"
+	heldz_items = 1
+	sewrepair = TRUE
 /*
 /obj/item/storage/belt/rogue/pickles
 	name = "jar of pickles"

code/modules/clothing/shoes/_shoes.dm
@@ -16,6 +16,7 @@
 	var/offset = 0
 	var/equipped_before_drop = FALSE
 	var/can_be_bloody = TRUE
+	var/isbarefoot = FALSE
 	bloody_icon_state = "shoeblood"
 
 /obj/item/clothing/shoes/ComponentInitialize()

commit 016e9f37e41ab37f5310d58d7252f3a39bf38305

Fix barefoot footstep sounds (#2773)
Fixes a bug introduced in #2345 where being barefoot no longer made
barefoot footstep sounds.

code/datums/components/footstep.dm
@@ -104,40 +104,42 @@
 	var/used_sound
 	var/list/used_footsteps
 
-	if(H.shoes || feetCover) //are we wearing shoes
-		var/obj/item/clothing/shoes/humshoes = H.shoes
-		if(humshoes.isbarefoot == FALSE) //are the shoes actually not barefoot
-			//SANITY CHECK, WILL NOT PLAY A SOUND IF THE LIST IS INVALID
-			if(!GLOB.footstep[T.footstep] || (LAZYLEN(GLOB.footstep[T.footstep]) < 3))
-				CRASH("Invalid footstep value. Turf type: [T.type]; Value: [T.footstep]")
-				return
-			used_footsteps = GLOB.footstep[T.footstep][1]
-			used_footsteps = used_footsteps.Copy()
-			used_sound = pick_n_take(used_footsteps)
-			if(used_sound == last_sound)
-				if(used_footsteps.len)
-					used_sound = pick(used_footsteps)
-			if(!used_sound)
-				used_sound = last_sound
-			last_sound = used_sound
-			playsound(T, used_sound,
-				GLOB.footstep[T.footstep][2],
-				FALSE,
-				GLOB.footstep[T.footstep][3] + e_range)
-		else
-			//SANITY CHECK, WILL NOT PLAY A SOUND IF THE LIST IS INVALID
-			if(!GLOB.barefootstep[T.barefootstep] || (LAZYLEN(GLOB.barefootstep[T.barefootstep]) < 3))
-				CRASH("Invalid barefootstep value given. Turf type: [T.type]; Value: [T.barefootstep]")
-				return
-			used_footsteps = GLOB.barefootstep[T.barefootstep][1]
-			used_footsteps = used_footsteps.Copy()
-			used_sound = pick_n_take(used_footsteps)
-			if(used_sound == last_sound)
+	var/obj/item/clothing/shoes/humshoes = H.shoes
+	// use the non-barefoot sound if:
+	// - we have shoes that either aren't barefoot or aren't of type /obj/item/clothing/shoes
+	// - our armour or pants are covering our feet
+	if((humshoes && (!istype(humshoes) || !humshoes.isbarefoot)) || feetCover)
+		//SANITY CHECK, WILL NOT PLAY A SOUND IF THE LIST IS INVALID
+		if(!GLOB.footstep[T.footstep] || (LAZYLEN(GLOB.footstep[T.footstep]) < 3))
+			CRASH("Invalid footstep value. Turf type: [T.type]; Value: [T.footstep]")
+			return
+		used_footsteps = GLOB.footstep[T.footstep][1]
+		used_footsteps = used_footsteps.Copy()
+		used_sound = pick_n_take(used_footsteps)
+		if(used_sound == last_sound)
+			if(used_footsteps.len)
 				used_sound = pick(used_footsteps)
-			if(!used_sound)
-				used_sound = last_sound
-			last_sound = used_sound
-			playsound(T, used_sound,
-				GLOB.barefootstep[T.barefootstep][2],
-				TRUE,
-				GLOB.barefootstep[T.barefootstep][3] + e_range)
+		if(!used_sound)
+			used_sound = last_sound
+		last_sound = used_sound
+		playsound(T, used_sound,
+			GLOB.footstep[T.footstep][2],
+			FALSE,
+			GLOB.footstep[T.footstep][3] + e_range)
+	else
+		//SANITY CHECK, WILL NOT PLAY A SOUND IF THE LIST IS INVALID
+		if(!GLOB.barefootstep[T.barefootstep] || (LAZYLEN(GLOB.barefootstep[T.barefootstep]) < 3))
+			CRASH("Invalid barefootstep value given. Turf type: [T.type]; Value: [T.barefootstep]")
+			return
+		used_footsteps = GLOB.barefootstep[T.barefootstep][1]
+		used_footsteps = used_footsteps.Copy()
+		used_sound = pick_n_take(used_footsteps)
+		if(used_sound == last_sound)
+			used_sound = pick(used_footsteps)
+		if(!used_sound)
+			used_sound = last_sound
+		last_sound = used_sound
+		playsound(T, used_sound,
+			GLOB.barefootstep[T.barefootstep][2],
+			TRUE,
+			GLOB.barefootstep[T.barefootstep][3] + e_range)