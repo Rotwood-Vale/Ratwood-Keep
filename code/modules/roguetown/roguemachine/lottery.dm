Rotwood-Vale/Ratwood-Keep

commit dacb830b40dda2bbe47e70a508555762ea7798a6

[Port] Xylix's machine lottery, gambling for gamblers (#1872)
<!-- Write **BELOW** The Headers and **ABOVE** The comments else it may
not be viewable. -->

## About The Pull Request

1. Ported lotterymachine from Azure Peak
https://github.com/GeneralPantsuIsBadAtCoding/Azure-Peak/pull/339
Works correctly without fractals or something else.


![image](https://github.com/user-attachments/assets/75becddd-d6f3-4c48-bc00-23ef0e6104ea)


2. map.Added SCOM and chest for seamster shop, fix some IDLOCK for rest
of chests on the map, added SCOM in the bastion's dungeon (not in the
cells).

## Why It's Good For The Game

GAAAAAAAAAAAMBLIIIIIIIIIIIIIIIING! And i don't know why seamster didn't
have a SCOM in his shop at the first place

code/modules/roguetown/roguemachine/lottery.dm
@@ -1,0 +1,254 @@
+/obj/structure/roguemachine/lottery_roguetown
+	name = "XYLIX'S FORTUNE"
+	desc = "An infinite, yawning hole that makes or breaks men. Come and play!"
+	icon = 'icons/roguetown/misc/machines.dmi'
+	icon_state = "lottery"
+	density = FALSE
+	pixel_y = 32
+	var/gamblingprice = 0
+	var/checkchatter = 0
+	var/chatterbox = 0
+
+//ensure these two are the same, or else first roll will be fucky
+	var/gamblingprob = 60
+	var/gamblingbaseprob = 60
+
+	var/diceroll = 100
+	var/maxtithing = 100
+	var/mintithing = 5
+	var/stopgambling = 0
+	var/probpenalty = 2
+	var/oldtithe = 0
+
+
+/obj/structure/roguemachine/lottery_roguetown/attack_hand(mob/living/user) //empty hand
+
+	src.say("Your current tithe is [src.gamblingprice] mammons. Care to spin?")
+	playsound(src, 'sound/misc/machinetalk.ogg', 100, FALSE, -1)
+	return
+
+/obj/structure/roguemachine/lottery_roguetown/attackby(obj/item/roguecoin/P, mob/living/user)
+
+	. = ..()
+
+	if(src.stopgambling == 1)
+		return
+
+	if(istype(P, /obj/item/roguecoin))
+		if(src.gamblingprice + (P.sellprice * P.quantity) > src.maxtithing)
+			say("This puts the starting tithe over [src.maxtithing] mammons.")
+			playsound(src, 'sound/misc/machineno.ogg', 100, FALSE, -1)
+			return
+		if(src.gamblingprice + (P.sellprice * P.quantity) < src.mintithing)
+			say("This is below [src.mintithing] mammons.")
+			playsound(src, 'sound/misc/machineno.ogg', 100, FALSE, -1)
+			return
+
+		else
+			src.gamblingprice += (P.sellprice * P.quantity)
+			qdel(P)
+			src.say("Your current tithe is now [src.gamblingprice] mammons. Care to spin?")
+			playsound(src, 'sound/misc/machinequestion.ogg', 100, FALSE, -1)
+			return
+
+
+/obj/structure/roguemachine/lottery_roguetown/MiddleClick(mob/living/user, params) //LET'S GO GAMBLING
+//checks - is it time to go gambling??
+	if(src.stopgambling == 1)
+		return
+	if(src.gamblingprice == 0)
+		src.say(pick("Eager fool; you need mammons to gamble your life away.", "You are missing your tithe.", "A lord without land is no lord at all."))
+		src.stopgambling = 1
+		sleep(20)
+		src.stopgambling = 0
+		return
+
+
+	else
+		src.diceroll = rand(1,100)
+		src.say(pick("Around and around I go, where I stop, only I know.", "Xylix smiles upon your idiocy, child.", "The wheel of fate spins, and spins.", "Oh, you poor fool.", "This is going to hurt for one of us.", "I laugh, you cry; I weep, you cheer..", "I will be your fool; I'll perform for you...", "Let's go gambling!", "Around and around, folly abounds.", "Dance with ruin and wealth."))
+		playsound(src, 'sound/misc/machinetalk.ogg', 100, FALSE, -1)
+		playsound(src, 'sound/misc/letsgogambling.ogg', 100, FALSE, -1)
+		src.gamblingprob += (user.STALUC - src.probpenalty)
+		src.stopgambling = 1
+
+		src.checkchatter -= 1
+
+//thug shaker
+		var/oldx = pixel_x
+		animate(src, pixel_x = oldx+1, time = 1)
+		animate(pixel_x = oldx-1, time = 1)
+		animate(pixel_x = oldx, time = 1)
+		sleep(50)
+
+//let's actually go gambling and determine results
+		if(src.gamblingprob > src.diceroll)
+			src.oldtithe = src.gamblingprice
+			src.gamblingprice *= pick(1.1, 1.1, 1.1, 1.1, 1.2, 1.2, 1.2, 1.4, 1.4, 2)
+			src.gamblingprice = round(src.gamblingprice)
+
+			peasant_betting()
+			letsgogamblinggamblers()
+			src.say(pick("Well-maneuvered, aristocrat! Your peasant's tithe is now [src.gamblingprice] mammons. Play again?", "A bountiful harvest, this year- the peasant's tithe rises to [src.gamblingprice] mammons. Spin me again?",))
+
+			playsound(src, 'sound/misc/machinetalk.ogg', 100, FALSE, -1)
+			src.gamblingprob = src.gamblingbaseprob
+			src.oldtithe = src.gamblingprice //this is redundant but i feel like bad things will happen if i don't do this :T
+			sleep(15)
+			src.stopgambling = 0
+			return
+
+		else
+			src.say(pick("TEN, WHEEL OF FORTUNE - inversed.", "The Castle. O, Omen!", "A harvest of locusts...!", "Look into my eyes and whisper your woes.", "Aw, dangit.", "Fool. Poor fool.", "Your eyes leak out of your skull, drool falling from your lips.", "Divine idiocy.", "You stand just as I did; loser and a freek."))
+			playsound(src, 'sound/misc/bug.ogg', 100, FALSE, -1)
+			sleep(20) //really make them THINK about their life choices up to this point
+			src.say(pick("King of fools, your land is barren. Play again?", "Divine comedy. Play again?", "Next time, surely. Play again?", "Haha-...ah-ha-ha! Again! Play again, jester!", "Poor beggar! Spin me again?"))
+			playsound(src, 'sound/misc/bug.ogg', 100, FALSE, -1)
+			src.gamblingprob = src.gamblingbaseprob
+			src.gamblingprice = 0
+			src.oldtithe = 0
+			sleep(15)
+			src.stopgambling = 0
+			return
+
+
+
+/obj/structure/roguemachine/lottery_roguetown/attack_right(mob/living/user) //how the fuck do i
+	. = ..()
+
+	if(!ishuman(user))
+		return
+	if(src.stopgambling == 1)
+		return
+
+	else
+		if(gamblingprice <= 0)
+			say("Poor thing, you are coinless.")
+			return
+		if(gamblingprice < 0)
+			say("Your peasant's tithe is NEGATIVE.")
+			return
+		var/list/choicez = list()
+		if(gamblingprice > 10)
+			choicez += "GOLD"
+		if(gamblingprice > 5)
+			choicez += "SILVER"
+		choicez += "BRONZE"
+		var/selection = input(user, "Make a Selection", src) as null|anything in choicez
+		if(!selection)
+			return
+		var/mod = 1
+		if(selection == "GOLD")
+			mod = 10
+		if(selection == "SILVER")
+			mod = 5
+		var/coin_amt = input(user, "Sayyid, you have [src.gamblingprice] mammon in tithes. You may withdraw [floor(gamblingprice/mod)] [selection] COINS.", src) as null|num
+		coin_amt = round(coin_amt)
+		if(coin_amt < 1)
+			return
+		if(!Adjacent(user))
+			return
+		if((coin_amt*mod) > gamblingprice)
+			playsound(src, 'sound/misc/machineno.ogg', 100, FALSE, -1)
+			return
+		else
+			budget2change(coin_amt*mod, user, selection)
+			gamblingprice -= coin_amt*mod
+
+
+
+/obj/structure/roguemachine/lottery_roguetown/proc/peasant_betting()
+
+	if(src.gamblingprice == oldtithe)
+		src.gamblingprice += pick(1,1,1,1,2,2)
+
+
+/obj/structure/roguemachine/lottery_roguetown/proc/letsgogamblinggamblers()
+
+	if(src.checkchatter > 1) //procs any time it's under 1
+		return
+
+	if(prob(90))
+		return
+
+	chatterbox = rand(1,12)
+
+	switch(chatterbox)
+		if(1)
+			src.say("I still remember the rain on my skin.")
+			playsound(src, 'sound/misc/machinetalk.ogg', 100, FALSE, -1)
+			sleep(30)
+			src.say("The wind in my fur...or was it hair? Either way...")
+			playsound(src, 'sound/misc/machinequestion.ogg', 100, FALSE, -1)
+		if(2)
+			src.say("The worship of gods is pernicious.")
+			playsound(src, 'sound/misc/machinetalk.ogg', 100, FALSE, -1)
+			sleep(20)
+			src.say("But this punishment is not as bad as others'! Ha-ha-ha!")
+			playsound(src, 'sound/misc/machinetalk.ogg', 100, FALSE, -1)
+		if(3)
+			src.say("There are fates worse than death...")
+			playsound(src, 'sound/misc/machinetalk.ogg', 100, FALSE, -1)
+			sleep(30)
+			src.say("...especially for a lowly fool who thought himself a king.")
+			playsound(src, 'sound/misc/bug.ogg', 100, FALSE, -1)
+		if(4)
+			src.say("She didn't realize Her machine would kill Her, of course.")
+			playsound(src, 'sound/misc/machinetalk.ogg', 100, FALSE, -1)
+			sleep(30)
+			src.say("...though 'tis difficult to argue what happened after that didn't benefit Her.")
+			playsound(src, 'sound/misc/bug.ogg', 100, FALSE, -1)
+		if(5)
+			src.say("Oh, Psydon?")
+			playsound(src, 'sound/misc/machinequestion.ogg', 100, FALSE, -1)
+			sleep(30)
+			src.say("To be honest, I'm about PSY-DONE with this whole debate! Ha-ha-h- ...No? Too soon? Alright.")
+			playsound(src, 'sound/misc/machinetalk.ogg', 100, FALSE, -1)
+		if(6)
+			src.say("You know, jester, those Ecclesials have the right idea.")
+			playsound(src, 'sound/misc/machinetalk.ogg', 100, FALSE, -1)
+			sleep(30)
+			src.say("Won't someone think of the deadite-loving, tax-hating, drug-using murderers?!")
+			playsound(src, 'sound/misc/bug.ogg', 100, FALSE, -1)
+		if(7)
+			src.say("...well, don't look at me for conversation.")
+			playsound(src, 'sound/misc/bug.ogg', 100, FALSE, -1)
+			sleep(30)
+			src.say("I've been the one doing all the chatting.")
+			playsound(src, 'sound/misc/machineno.ogg', 100, FALSE, -1)
+		if(8)
+			src.say("Can't you smell the stench in the air? It's terrible.")
+			playsound(src, 'sound/misc/bug.ogg', 100, FALSE, -1)
+			sleep(30)
+			src.say("It wasn't nearly so bad, before. Rot and puss. Oh, well.")
+			playsound(src, 'sound/misc/bug.ogg', 100, FALSE, -1)
+		if(9)
+			src.say("Can't you smell the stench in the air, fool? It's terrible.")
+			playsound(src, 'sound/misc/bug.ogg', 100, FALSE, -1)
+			sleep(30)
+			src.say("I don't know how you could miss it. Rot and puss. Oh, well.")
+			playsound(src, 'sound/misc/bug.ogg', 100, FALSE, -1)
+		if(10)
+			src.say("Maybe you ought stop while you are ahead, jester.")
+			playsound(src, 'sound/misc/machinetalk.ogg', 100, FALSE, -1)
+			sleep(30)
+			src.say("...greed is what got your lot into this mess, after all.")
+			playsound(src, 'sound/misc/bug.ogg', 100, FALSE, -1)
+		if(11)
+			src.say("A father and his son are riding a carriage through a forrest. Suddenly, Z's curse! The axle snaps!")
+			playsound(src, 'sound/misc/machinetalk.ogg', 100, FALSE, -1)
+			sleep(30)
+			src.say("The father dies, but the son- the son yet lives! He's brought to the nearby village's physician.")
+			playsound(src, 'sound/misc/machinetalk.ogg', 100, FALSE, -1)
+			sleep(30)
+			src.say("Upon seeing him, the physician ga-...what do you mean, you've heard this one before?")
+			playsound(src, 'sound/misc/bug.ogg', 100, FALSE, -1)
+		else
+			src.say("Me? Am I anybody important...? Oh, no.")
+			playsound(src, 'sound/misc/machineyes.ogg', 100, FALSE, -1)
+			sleep(25)
+			src.say("I am nothing but a lowly jester, just like you! Ha-ha-ha!")
+			playsound(src, 'sound/misc/bug.ogg', 100, FALSE, -1)
+
+	sleep(40)
+	src.checkchatter = rand(1,11) //hope he doesn't have pocket aces

_maps/map_files/rockhill/rockhill.dmm
@@ -2994,6 +2994,10 @@
 /obj/structure/mineral_door/wood,
 /turf/open/floor/rogue/cobble,
 /area/rogue/indoors/town/tavern)
+"ctT" = (
+/obj/structure/roguemachine/scomm/l,
+/turf/open/floor/rogue/wood,
+/area/rogue/indoors/town)
 "cuk" = (
 /turf/open/floor/rogue/concrete,
 /area/rogue/indoors)
@@ -3922,7 +3926,9 @@
 /turf/open/floor/rogue/cobble,
 /area/rogue/indoors/town/dwarfin)
 "dce" = (
-/obj/structure/closet/crate/drawer,
+/obj/structure/closet/crate/drawer{
+	lockid = "bog_gatehouse"
+	},
 /obj/item/natural/cloth,
 /obj/item/reagent_containers/glass/bottle,
 /turf/open/floor/rogue/blocks,
@@ -4589,6 +4595,11 @@
 /obj/effect/landmark/start/gravedigger,
 /turf/open/floor/rogue/churchrough,
 /area/rogue/indoors/town/church/chapel)
+"dBz" = (
+/obj/structure/roguemachine/lottery_roguetown,
+/obj/structure/chair/wood,
+/turf/open/floor/carpet/royalblack,
+/area/rogue/indoors/town/tavern)
 "dBG" = (
 /turf/closed/wall/mineral/rogue/decostone/mossy/red,
 /area/rogue/indoors/town/church)
@@ -8092,7 +8103,9 @@
 /turf/open/floor/rogue/cobble,
 /area/rogue/under/town/basement)
 "ghN" = (
-/obj/structure/closet/crate/chest,
+/obj/structure/closet/crate/chest{
+	lockid = "bog_gatehouse"
+	},
 /turf/open/floor/rogue/cobble/mossy,
 /area/rogue/under/town/basement)
 "ghO" = (
@@ -8615,6 +8628,7 @@
 /area/rogue/indoors/town/church)
 "gDi" = (
 /obj/effect/decal/cleanable/blood,
+/obj/structure/roguemachine/scomm/l,
 /turf/open/floor/rogue/cobblerock,
 /area/rogue/under/town/basement)
 "gDx" = (
@@ -9681,7 +9695,7 @@
 /area/rogue/indoors/town/bath)
 "hvk" = (
 /obj/structure/closet/crate/chest{
-	lockid = "keep_armory"
+	lockid = "steward"
 	},
 /obj/item/natural/cloth,
 /turf/open/floor/rogue/ruinedwood{
@@ -9771,9 +9785,6 @@
 	pixel_x = -23;
 	pixel_y = 4
 	},
-/obj/item/powderflask,
-/obj/item/ammo_holder/bullet/lead,
-/obj/item/gun/ballistic/handgonne,
 /turf/open/floor/carpet/royalblack,
 /area/rogue/indoors)
 "hAg" = (
@@ -11494,7 +11505,9 @@
 /turf/open/floor/rogue/hexstone,
 /area/rogue/indoors/town/church)
 "iNA" = (
-/obj/structure/closet/crate/drawer,
+/obj/structure/closet/crate/drawer{
+	lockid = "bog_gatehouse"
+	},
 /obj/item/natural/cloth,
 /obj/item/reagent_containers/glass/bottle,
 /obj/machinery/light/rogue/wallfire/candle,
@@ -12225,7 +12238,8 @@
 /area/rogue/indoors/town/church)
 "jmB" = (
 /obj/structure/closet/crate/chest/gold{
-	lockid = "bog_master"
+	lockid = "bog_master";
+	locked = 1
 	},
 /obj/machinery/light/rogue/torchholder/r,
 /obj/item/key/bog_gatehouse,
@@ -12234,6 +12248,9 @@
 /obj/item/key/bog_gatehouse,
 /obj/item/key/bog_gatehouse,
 /obj/item/key/bog_master,
+/obj/item/gun/ballistic/handgonne,
+/obj/item/ammo_holder/bullet/lead,
+/obj/item/powderflask,
 /turf/open/floor/rogue/herringbone,
 /area/rogue/indoors)
 "jod" = (
@@ -17576,7 +17593,9 @@
 /turf/open/floor/carpet/red,
 /area/rogue/indoors/town/tavern)
 "nxK" = (
-/obj/structure/closet/crate/roguecloset,
+/obj/structure/closet/crate/roguecloset{
+	lockid = "steward"
+	},
 /turf/open/floor/rogue/ruinedwood,
 /area/rogue/indoors/town/shop)
 "nxU" = (
@@ -17610,6 +17629,10 @@
 "nyP" = (
 /obj/structure/rack/rogue,
 /turf/open/floor/rogue/ruinedwood/turned,
+/area/rogue/indoors)
+"nyQ" = (
+/obj/structure/roguemachine/scomm/l,
+/turf/open/floor/rogue/concrete,
 /area/rogue/indoors)
 "nyR" = (
 /obj/structure/rack/rogue,
@@ -20827,7 +20850,9 @@
 /turf/open/floor/rogue/dirt/road,
 /area/rogue/outdoors/rtfield)
 "pPW" = (
-/obj/structure/closet/crate/roguecloset/dark,
+/obj/structure/closet/crate/roguecloset/dark{
+	lockid = "steward"
+	},
 /turf/open/floor/rogue/ruinedwood,
 /area/rogue/indoors/town/shop)
 "pQd" = (
@@ -22779,6 +22804,7 @@
 	icon_state = "largetable"
 	},
 /obj/item/rogueweapon/stoneaxe/woodcut,
+/obj/item/rogueweapon/stoneaxe/woodcut,
 /turf/open/floor/rogue/ruinedwood,
 /area/rogue/indoors/town)
 "ruk" = (
@@ -24376,7 +24402,9 @@
 /turf/open/floor/rogue/ruinedwood,
 /area/rogue/indoors/town/shop)
 "sFm" = (
-/obj/structure/closet/crate/chest,
+/obj/structure/closet/crate/chest{
+	lockid = "bog_gatehouse"
+	},
 /obj/item/rope/chain,
 /obj/item/rope/chain,
 /turf/open/floor/rogue/concrete,
@@ -24881,6 +24909,10 @@
 /obj/machinery/light/rogue/wallfire/candle/l,
 /turf/open/floor/rogue/wood,
 /area/rogue/indoors/town/church)
+"sZv" = (
+/obj/machinery/light/rogue/firebowl,
+/turf/open/floor/rogue/dirt,
+/area/rogue/outdoors/rtfield)
 "sZw" = (
 /turf/open/floor/rogue/rooftop,
 /area/rogue/outdoors/rtfield)
@@ -27111,7 +27143,9 @@
 /turf/open/floor/rogue/cobblerock,
 /area/rogue/outdoors/rtfield)
 "uHW" = (
-/obj/structure/closet/crate/roguecloset,
+/obj/structure/closet/crate/roguecloset{
+	lockid = "steward"
+	},
 /obj/item/reagent_containers/food/snacks/smallrat,
 /turf/open/floor/rogue/hexstone,
 /area/rogue/indoors/town/manor)
@@ -27481,6 +27515,8 @@
 	pixel_x = 0;
 	pixel_y = -32
 	},
+/obj/item/natural/bundle/fibers/full,
+/obj/item/natural/bundle/fibers/full,
 /turf/open/floor/rogue/wood,
 /area/rogue/indoors/town)
 "uTW" = (
@@ -28185,7 +28221,7 @@
 /area/rogue/indoors/town)
 "vvV" = (
 /obj/structure/closet/crate/chest{
-	lockid = "keep_armory"
+	lockid = "steward"
 	},
 /obj/item/reagent_containers/food/snacks/rogue/foodbase/hardtack_raw/cooked,
 /obj/machinery/light/rogue/torchholder{
@@ -28899,6 +28935,12 @@
 	icon_state = "roofg"
 	},
 /area/rogue/outdoors/town)
+"vYW" = (
+/obj/structure/closet/crate/chest{
+	lockid = "seamster"
+	},
+/turf/open/floor/rogue/wood,
+/area/rogue/indoors/town)
 "vZb" = (
 /turf/closed/wall/mineral/rogue/roofwall/outercorner{
 	dir = 4
@@ -29441,7 +29483,7 @@
 /area/rogue/indoors)
 "wuB" = (
 /obj/structure/closet/crate/chest{
-	lockid = "keep_armory"
+	lockid = "steward"
 	},
 /turf/open/floor/rogue/ruinedwood,
 /area/rogue/indoors/town/shop)
@@ -31015,6 +31057,14 @@
 "xAt" = (
 /turf/open/transparent/openspace,
 /area/rogue/outdoors/town)
+"xAy" = (
+/obj/structure/closet/crate/drawer{
+	lockid = "bog_gatehouse"
+	},
+/obj/item/natural/cloth,
+/obj/item/reagent_containers/glass/bottle,
+/turf/open/floor/rogue/herringbone,
+/area/rogue/under/town/basement)
 "xAL" = (
 /obj/effect/decal/cleanable/cobweb{
 	icon_state = "cobweb2"
@@ -184080,7 +184130,7 @@ iTw
 cTA
 kHr
 wHU
-tuM
+xAy
 cTA
 lLK
 lLK
@@ -184884,7 +184934,7 @@ fNe
 cTA
 wnV
 wHU
-tuM
+xAy
 cTA
 lLK
 lLK
@@ -270146,7 +270196,7 @@ spp
 qvR
 wSn
 wSn
-uaL
+fTe
 qbG
 qbG
 qbG
@@ -285004,7 +285054,7 @@ qvR
 uaL
 qvR
 uaL
-wSn
+sZv
 fxx
 fxx
 fxx
@@ -291842,7 +291892,7 @@ fqj
 lVe
 qvR
 sjP
-qvR
+gex
 fxx
 fxx
 fxx
@@ -299765,7 +299815,7 @@ vAB
 edc
 orb
 orb
-mdo
+dBz
 mdo
 szD
 cEg
@@ -300997,7 +301047,7 @@ qEz
 tGY
 agt
 ruc
-ucS
+ugJ
 nHb
 ucS
 qEz
@@ -305809,7 +305859,7 @@ wbg
 wVI
 ndD
 nDD
-rNm
+ctT
 thT
 cyJ
 nqr
@@ -307420,7 +307470,7 @@ hMg
 nqr
 thT
 qka
-rNm
+vYW
 rNm
 vce
 evo
@@ -394723,7 +394773,7 @@ pAV
 pAV
 tQE
 cuk
-cuk
+nyQ
 wlD
 wlD
 cuk

roguetown.dme
@@ -1768,6 +1768,7 @@
 #include "code\modules\roguetown\roguemachine\bounty.dm"
 #include "code\modules\roguetown\roguemachine\camera.dm"
 #include "code\modules\roguetown\roguemachine\drugmachine.dm"
+#include "code\modules\roguetown\roguemachine\lottery.dm"
 #include "code\modules\roguetown\roguemachine\hoardmaster.dm"
 #include "code\modules\roguetown\roguemachine\mail.dm"
 #include "code\modules\roguetown\roguemachine\merchant.dm"

commit b48244eb587e3d72ba81563c6efd52c1568b4b92

Fix exploit in lottery machine (#2775)
<!-- Write **BELOW** The Headers and **ABOVE** The comments else it may
not be viewable. -->

## About The Pull Request

Fixes a bug introduced in #1872

1) Insert gold
2) Right click the machine to open the withdraw dialog
3) Keep it open and middle click to gamble
4) Withdraw money on when fail sound effect triggers

This adds an additional check before coins withdraw to see if gambling
is currently underway.

## Why It's Good For The Game

Closes an exploit for infinite coins.

## Proof of Testing (Required)

I have tested this in a local server and confirm it fixes the bug.

code/modules/roguetown/roguemachine/lottery.dm
@@ -148,6 +148,8 @@
 			return
 		if(!Adjacent(user))
 			return
+		if(src.stopgambling == 1) // double check because it's possible to have input field open before starting gambling
+			return
 		if((coin_amt*mod) > gamblingprice)
 			playsound(src, 'sound/misc/machineno.ogg', 100, FALSE, -1)
 			return